d09e53666d62efe298c795951307d953caf96f54
admin <zhangsan@itcast.cn>
2020-05-16 18:40:15 +0800

    修改firstclass

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -2,10 +2,14 @@ package com.itheima.git;
 
 public class FirstClass {
     public static void main(String[] args) {
+
         System.out.println("hello git progect");
+        //hello test
     }
-    public void test(){
-        int i=0;
-        i +=10;
+
+    public void test() {
+        int i = 0;
+        i += 10;
+        System.out.println(i);
     }
 }
